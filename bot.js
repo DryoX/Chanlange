cb4561f327c7a50adef8f958781e2c5fc516441d
DryoX <40540721+DryoX@users.noreply.github.com>
2018-12-01 22:46:14 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'hei') {
+    	message.reply('TAYO');
   	}
 });
 
